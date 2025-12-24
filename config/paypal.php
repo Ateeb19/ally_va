71f703e7c9915f292467842992de1d2aeece0d62
Ateeb19 <ateebhaque1912@gmail.com>
2025-12-24 21:18:18 +0530

    update

--- config/paypal.php
+++ config/paypal.php
@@ -5,10 +5,10 @@
  */
 
 return [
-    'mode'    => env('PAYPAL_MODE', 'sandbox'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
+    'mode'    => env('PAYPAL_MODE', 'live'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
     'sandbox' => [
-        'client_id'         => env('PAYPAL_SANDBOX_CLIENT_ID'),
-        'client_secret'     => env('PAYPAL_SANDBOX_CLIENT_SECRET'),
+        'client_id'         => env('PAYPAL_LIVE_CLIENT_ID'),
+        'client_secret'     => env('PAYPAL_LIVE_CLIENT_SECRET'),
         // 'app_id'            => 'APP-80W284485P519543T',
         'app_id'            => '',
     ],
