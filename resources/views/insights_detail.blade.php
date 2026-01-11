e30d00ad2e2df2969124c394d12938538ef95a55
arupseth <arupkseth@gmail.com>
2026-01-11 12:44:44 +0530

    Update insights_detail.blade.php

--- resources/views/insights_detail.blade.php
+++ resources/views/insights_detail.blade.php
@@ -10,10 +10,7 @@
         <div class="container">
           <h1>Insights That Work as Hard as You Do</h1>
           <p>
-            Feeling overwhelmed by your endless to-do list? Your solution is
-            closer than you think. These insights are your playbook for smart
-            delegation—designed to help you reclaim time, simplify your
-            workflow, and unlock the growth you’ve been working so hard for.
+            Feeling overwhelmed by your to do list? The right support is closer than you think. These insights help you delegate smarter, save time, simplify your workflow, and focus on growing your business.
           </p>
         </div>
       </div>
