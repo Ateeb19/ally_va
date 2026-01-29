4e1e126af07297f7b9f69a611f8db4a5f82aef1a
Ateeb19 <ateebhaque1912@gmail.com>
2026-01-29 19:04:11 +0530

    update

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,6 +1,33 @@
-@extends('layouts.app')
-@section('page not found', 'page not found')
-@section('content')
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>page not found</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet" />
+
+    <!-- Your CSS -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <!-- <link rel="icon" type="image/png" href="images/ally-va-favicon.png" /> -->
+    <link rel="icon" type="image/png" href="{{ asset('images/ally-va-favicon.png') }}">
+    <!-- Bootstrap & Swiper (optional) -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
     <section class="nopage-wrapper">
         <div class="container">
             <div class="row">
@@ -9,7 +36,8 @@
                         <circle cx="50" cy="50" r="45" stroke="#288feb" stroke-width="4" />
                         <circle cx="35" cy="40" r="4" fill="#288feb" />
                         <circle cx="65" cy="40" r="4" fill="#288feb" />
-                        <line x1="35" y1="65" x2="65" y2="65" stroke="#288feb" stroke-width="4" stroke-linecap="round" />
+                        <line x1="35" y1="65" x2="65" y2="65" stroke="#288feb" stroke-width="4"
+                            stroke-linecap="round" />
                     </svg>
 
                     <h1>Oops! We couldn't find that page</h1>
@@ -22,5 +50,6 @@
             </div>
         </div>
     </section>
+</body>
 
-@endsection
+</html>
