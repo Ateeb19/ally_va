686bfb21f7ead79c6339104f6c2a4610e7d08ef9
Ateeb19 <ateebhaque1912@gmail.com>
2026-02-11 15:59:29 +0530

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use App\Notifications\ResetPasswordNotification;
 
 class User extends Authenticatable
 {
@@ -44,4 +45,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
+
 }
--- resources/views/vendor/mail/html/footer.blade.php
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -1,4 +1,4 @@
-<tr>
+<!-- <tr>
 <td>
 <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
 <tr>
@@ -7,5 +7,13 @@
 </td>
 </tr>
 </table>
+</td>
+</tr> -->
+
+<tr>
+<td class="footer" style="text-align: center; padding: 20px;">
+<p style="font-size: 12px; color: #6c757d;">
+© {{ date('Y') }} Allyva. All rights reserved.
+</p>
 </td>
 </tr>
